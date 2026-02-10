74e80c084aaaafaf677b30d3d432a8bef03c03c5
Chelsea Winata <cwinata042@gmail.com>
2026-02-10 18:19:27 -0500

    added some data types

---
+++ lib/types/freesr.ts
@@ -0,0 +1,38 @@
+import chars from "../../data/game_data.json";
+
+export type TCharacterId = keyof typeof chars.characters;
+
+export type TCharacter = {
+  id: TCharacterId;
+  name: string;
+  rarity: number;
+  path: string;
+  element: string;
+  traces: any;
+  traceTree: any;
+  max_sp: number;
+  stats: {
+    HP: number;
+    ATK: number;
+    DEF: number;
+    SPD: number;
+    CRIT_Rate: number;
+    CRIT_DMG: number;
+  };
+  unreleased: boolean;
+};
+
+export type TFreeSRChar = {
+  avatar_id: TCharacterId; // Character ID
+  data: {
+    rank: number; // Eidolon level
+    skills: object; // Collection of Skill IDs and current level
+    skills_by_anchor_type: object;
+  };
+  level: number; // Character level
+  promotion: number; // Ascension value
+  sp_value: number; // Current Energy
+  sp_max: number; // Max Energy
+  techniques: number[]; // Technique IDs
+  enhanced_id: number | null; // Novaflare, 1 if enhanced, null if not
+};
