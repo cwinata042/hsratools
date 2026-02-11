d4d08dbd17eaa15ccd7f793400412e697abb8a84
Chelsea Winata <cwinata042@gmail.com>
2026-02-10 19:26:50 -0500

    css for buttons and base

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,27 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { IoMdSettings } from "react-icons/io";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="main">
+      <div className="body">
+        <div className="characters">
+          <div className="character-nav">Character Navigation</div>
+          <div className="character-main">Main Character Screen</div>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </div>
+      <div className="navbar">
+        <div className="navbar-group">
+          <button className="circle">
+            <IoMdSettings />
+          </button>
+          <button>Download freesr-data.json</button>
+          <button>Connect to PS</button>
         </div>
-      </main>
+        <div className="navbar-group">
+          <button>Characters</button>
+          <button>Enemy Selection</button>
+        </div>
+      </div>
     </div>
   );
 }
