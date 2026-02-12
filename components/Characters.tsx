c1004eea126a6ff9b39615adde1061a2cc14a250
Chelsea Winata <cwinata042@gmail.com>
2026-02-12 00:41:57 -0500

    character details header

--- components/Characters.tsx
+++ components/Characters.tsx
@@ -9,9 +9,11 @@ export default function Characters() {
   const chars: TCharacterDetails[] = Object.values(game_data.characters);
   const scrollRef = useHorizontalScroll();
 
+  const currChar = chars.filter((char) => char.name === "Phainon")[0];
+
   const charIcons = chars.map((char) => {
     return (
-      <div key={char.id} className="char-icon">
+      <div key={char.id} className="large icon">
         <Image
           src={`/assets/icon/avatar/${char.id}.webp`}
           alt={`Character icon`}
@@ -27,10 +29,51 @@ export default function Characters() {
 
   return (
     <div className="characters">
-      <div ref={scrollRef} className="character-nav no-scrollbar drag-scroll">
-        {charIcons}
+      <div className="character-header">
+        <div className="character-header-section">
+          <div className="medium icon">
+            <Image
+              src={`/assets/icon/ui/Character_Gold.png`}
+              alt={`Character icon`}
+              fill
+              sizes="(max-width: 60px) 100vw"
+              style={{ objectFit: "cover" }}
+              className="unselectable"
+              draggable={false}
+            />
+          </div>
+          <div className="character-info-container">
+            <p className="gold nowrap">Character Details</p>
+            <div className="character-info">
+              <div className="small icon">
+                <Image
+                  src={`/assets/icon/path/${currChar.path}_Simple.webp`}
+                  alt={`${currChar.path} icon`}
+                  fill
+                  sizes="(max-width: 22px) 100vw"
+                  style={{ objectFit: "cover" }}
+                  className="unselectable"
+                  draggable={false}
+                />
+              </div>
+              <p className="medium horizontal">
+                <p className="nowrap">{currChar.path}</p>
+                <p>/</p>
+                <p>{currChar.name}</p>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div ref={scrollRef} className="character-nav no-scrollbar drag-scroll">
+          {charIcons}
+        </div>
+        <div className="character-header-section"></div>
       </div>
-      <div className="character-main">Main Character Screen</div>
+
+      <div className="character-main">
+        <div>Tabs</div>
+        <div>tab specific content</div>
+      </div>
     </div>
   );
 }
