8d46227070e8074befa50492b1b6fddb55d494f9
konburuu <cwinata042@gmail.com>
2026-05-02 00:20:50 -0400

    add types

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
+"use client"
+
 import Characters from "@/components/Characters";
+import { TDatabase } from "@/lib/types/freesr";
 import { IoMdSettings } from "react-icons/io";
 import { IoDownloadOutline } from "react-icons/io5";
 import { VscDebugDisconnect } from "react-icons/vsc";
 
 export default function Home() {
+  // Initializes DB 0 if does not exist
+  if (!localStorage.getItem("db")) {
+    initDatabase()
+  }
+
+  function initDatabase() {
+    let data : TDatabase = {
+      avatars: {},
+      battle_config: {
+        battle_type: "DEFAULT",
+        blessings: [],
+        custom_stats: [],
+        cycle_count: 0,
+        monsters: [],
+        path_resonance_id: 0,
+        stage_id: 0
+      },
+      lightcones: [],
+      loadout: [],
+      name: "Database 1",
+      relics: []
+    }
+
+    localStorage.setItem("db", JSON.stringify([data]))
+  }
+
   return (
     <div className="main">
       <div className="body">
--- lib/types/freesr.ts
+++ lib/types/freesr.ts
@@ -18,8 +18,8 @@ export type TCharacterDetails = {
   unreleased: boolean;
 };
 
-export type TFreeSRChar = {
-  avatar_id: string; // Character ID
+export type TAvatar = {
+  avatar_id: number; // Character ID
   data: {
     rank: number; // Eidolon level
     skills: object; // Collection of Skill IDs and current level
@@ -32,3 +32,70 @@ export type TFreeSRChar = {
   techniques: number[]; // Technique IDs
   enhanced_id: number | null; // Novaflare, 1 if enhanced, null if not
 };
+
+export type TDatabase = {
+  avatars: any;
+  battle_config: TBattleConfig;
+  name: string;
+  lightcones: TLightCone[];
+  loadout: TLoadout[];
+  relics: TRelic[];
+}
+
+export type TRelic = {
+  internal_uid: number;
+  relic_id: number;
+  relic_set_id: number;
+  equip_avatar: number;
+  level: number;
+  main_affix_id: number;
+  sub_affixes: TSubAffix[];
+}
+
+export type TSubAffix = {
+  count: number;
+  step: number;
+  sub_affix_id: number;
+  rolls: number[];
+}
+
+export type TLightCone = {
+  internal_uid: number;
+  item_id: number;  // light_cone_ranks.json key
+  equip_avatar: number; // game_data characters key, 0 if unequipped
+  level: number;
+  promotion: number;
+  rank: number; // superimposition level
+}
+
+export type TLoadout = {
+  name: string;
+  char_id: number; // game_data characters key
+  head: number; // 0 if none, else relic uid
+  hand: number;
+  body: number;
+  feet: number;
+  planar_sphere: number;
+  link_rope: number;
+}
+
+export type TBattleConfig = {
+  battle_type: string;
+    blessings: TBlessing[],
+    custom_stats: any;
+    cycle_count: number;
+    monsters: TMonster[]
+    path_resonance_id: number;
+    stage_id: number;
+}
+
+export type TBlessing = {
+  level: number;
+  id: number;
+}
+
+export type TMonster = {
+  monster_id: number;
+  amount: number;
+  level: number;
+}
